05e77c957ecd11112c96577ac9df15e8aa22c8c1
Jati <gajikoe@gmail.com>
2019-08-25 11:04:16 +0700

    add rollback

--- resources/rtes.js
+++ resources/rtes.js
@@ -5,10 +5,17 @@ var dbasync = require('../db/dbasync');
 router.post('/bayar', async function(req, res) {
     let ntpn = ''
     try {
+        //must using transaction
         await dbasync.myexec("START TRANSACTION");
-        //select billing
+        //select billing with FOR UPDATE => t
+        //this is will lock row until update, second request will be blocked
         let data = await dbasync.myexec(`SELECT * FROM m_billing WHERE BILLINGCODE="` +
-            req.body.BILLINGCODE + `" AND PAYMENTSTATUS=0`);
+            req.body.BILLINGCODE + `" AND PAYMENTSTATUS=0 FOR UPDATE`);
+
+        //if no data = rollback
+        if (data.length == 0) {
+            await dbasync.myexec("ROLLBACK");
+        }
 
         //generate ntpn
         ntpn = await new Promise(resolve => setTimeout(resolve('12345'), 5000));
@@ -18,7 +25,8 @@ router.post('/bayar', async function(req, res) {
         VALUES("` + data[0].BILLINGCODE + `",` + data[0].BILLINGAMOUNT + `,"` + ntpn + `")`);
 
         //update status
-        await dbasync.myexec(`UPDATE m_billing SET PAYMENTSTATUS=1 WHERE BILLINGCODE="` + req.body.BILLINGCODE + `"`);
+        await dbasync.myexec(`UPDATE m_billing SET PAYMENTSTATUS=1 WHERE BILLINGCODE="` +
+            req.body.BILLINGCODE + `"`);
 
         await dbasync.myexec("COMMIT");
         res.status(200).json({
